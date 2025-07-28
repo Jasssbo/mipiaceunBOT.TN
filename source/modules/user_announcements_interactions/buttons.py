46640bd3b5d53fbb60be321d12ef2992a21032fb
Jasssbo <152605009+Jasssbo@users.noreply.github.com>
2025-07-28 21:59:34 +0200

    Fix 10.1.0

--- source/modules/user_announcements_interactions/buttons.py
+++ source/modules/user_announcements_interactions/buttons.py
@@ -5,7 +5,7 @@ import logging
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from config import CATEGORY_QUESTIONS, user_data, bot, CHAT_ID, YELLOW, GREEN, RED, RESET
 from modules.user_announcements_interactions.announcement_compiler import send_clean_message, send_preview, update_preview_with_id, publish_announcement, is_user_allowed_by_username, safe_delete
-from source.modules.topic_guardian import is_user_allowed_by_username
+from modules.topic_guardian import is_user_allowed_by_username
 
 # ------------------------ BUTTONS CALLBACK HANDLER ------------------------
 # buttons_callback_handler: Gestisce tutte le interazioni con i bottoni InlineKeyboard, 
